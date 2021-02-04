9bd96f7a70e46bb71c03ce166d0b82c873b4ddf7
TiagoLogarinho <tiagologarinho@gmail.com>
2021-02-04 22:12:12 +0000

    Initial Test
    Default Linear Regression on initial data, 99.9% accuracy

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn import linear_model
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.utils import shuffle
 ###########################################
 
 data = pd.read_csv('Datasets/archive/bitcoin_price.csv', )
-print(data.head)
+
+x = data.drop(['Close','Date','Volume','Market Cap'], axis = 1).values
+y = data['Close'].values
+print(x.shape)
+print(y.shape)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.3, random_state=0)
+model = LinearRegression()
+model.fit(x_train, y_train)
+y_train_pred = model.predict(x_train)
+y_test_pred = model.predict(x_test)
+print("Training score: {:.3f}".format(model.score(x_train, y_train)))
+print("Testing score: {:.3f}".format(model.score(x_test, y_test)))
