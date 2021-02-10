3d7d2d92d00f3b410969538e118614164aa0dab2
TiagoLogarinho <tiagologarinho@gmail.com>
2021-02-10 11:21:58 +0000

    Initial Plotting

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ data = pd.read_csv('Datasets/archive/bitcoin_price.csv', )
 
 x = data.drop(['Close','Date','Volume','Market Cap'], axis = 1).values
 y = data['Close'].values
-print(x.shape)
-print(y.shape)
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.3, random_state=0)
 model = LinearRegression()
 model.fit(x_train, y_train)
@@ -22,3 +20,8 @@ y_train_pred = model.predict(x_train)
 y_test_pred = model.predict(x_test)
 print("Training score: {:.3f}".format(model.score(x_train, y_train)))
 print("Testing score: {:.3f}".format(model.score(x_test, y_test)))
+print("Actual Test labels: {0}".format(y_test[:5]))
+print("Predicted labels: {0}".format(y_test_pred[:5]))
+plt.plot(y_test)
+plt.plot(y_test_pred)
+plt.show()
